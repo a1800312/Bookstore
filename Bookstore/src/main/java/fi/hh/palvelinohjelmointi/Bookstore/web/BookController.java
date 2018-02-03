16c1b179ffe80b460bb00f7a1c4f713f8df702df
a1800312 <35593887+a1800312@users.noreply.github.com>
2018-02-04 00:10:28 +0200

    Add files via upload
    Tehtävä 3.2 valmis

--- Bookstore/src/main/java/fi/hh/palvelinohjelmointi/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/fi/hh/palvelinohjelmointi/Bookstore/web/BookController.java
@@ -1,15 +1,28 @@
 package fi.hh.palvelinohjelmointi.Bookstore.web;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.hh.palvelinohjelmointi.Bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
+	
+	@Autowired
+	private BookRepository repository;
 
 	@RequestMapping(value="/index", method=RequestMethod.GET)
 	public String indexRequest(){
 		// do something
 		return "index";
+	}
+	
+	@RequestMapping(value="/booklist", method=RequestMethod.GET)
+	public String booksListed(Model model){
+			model.addAttribute("books", repository.findAll());
+			return "booklist";
 	}
 	
 }
